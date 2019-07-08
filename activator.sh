f424fd42be4231c3fabdf706667aa5cc34c1e689
SAR818 <52664977+SAR818@users.noreply.github.com>
2019-07-08 15:36:29 +0000

    Add files via upload

---
+++ activator.sh
@@ -0,0 +1,24 @@
+echo 'SELAMAT DATANG DI CONTENT TZFA KAMI'
+=========================================
+
+
+echo -n 'MASUKAN LINSENSI UNTUK MEMBUKA LINK : '
+read pw;
+
+if [ $pw = 'bCx-Jhg-Kai' ];
+then
+   echo 'LISENSI BENAR'
+else
+   echo 'LISENSI SALAH'
+   echo 'ANDA BISA MEMBELI VIA WA 0895367086087'
+fi
+
+
+echo 'link ; '
+echo 'link ; '
+echo 'link ; '
+echo 'link ; '
+echo 'link ; '
+
+
+echo  'DILARANG MEMBAJAK/MENYEBARKAN CONTEN INI'
